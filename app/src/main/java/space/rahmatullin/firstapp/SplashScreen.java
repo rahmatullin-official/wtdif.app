7fff4944066d0df99f2d7a070bc113c8335ca17a
rahmatullin-official <damir-developer@yandex.ru>
2022-05-10 22:13:57 +0300

    add auth with firebase to app

---
+++ app/src/main/java/space/rahmatullin/firstapp/SplashScreen.java
@@ -0,0 +1,19 @@
+package space.rahmatullin.firstapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+
+@SuppressLint("CustomSplashScreen")
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
